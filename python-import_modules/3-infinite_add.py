11f4168c26f08e534f0cf420ab96d2979b4c28df
root <felipeoliveramantero@gmail.com>
2024-01-28 22:27:02 +0000

    task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+
+    else argc != 0:
+        sum = 0
+        for i in range(1, argc, + 1)
+            sum += int(sys.argv[i])
+
+        print(sum)
