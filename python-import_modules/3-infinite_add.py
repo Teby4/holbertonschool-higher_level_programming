a28af8451d42e974c851298867877d5f85367031
root <felipeoliveramantero@gmail.com>
2024-01-28 22:29:34 +0000

    task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     else:
         sum = 0
-        for i in range(1, argc, + 1)
+        for i in range(1, argc, + 1):
             sum += int(sys.argv[i])
 
         print(sum)
