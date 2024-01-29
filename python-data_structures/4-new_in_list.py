cc5529fdf72bb6f903d723b2083e3178b2f4a82e
root <felipeoliveramantero@gmail.com>
2024-01-29 01:22:49 +0000

    task 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if idx < 0
+    if idx < 0:
         return my_list
     if idx < len(new_list):
         new_list[idx] = element
