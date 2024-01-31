5b64eda9a057b98e7e95a566980200fcd8f3f53d
Teby4 <felipeoliveramantero@gmail.com>
2024-01-31 05:36:03 -0800

    task 1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list
-    for i in new_list
+    new_list = ""
+    for i in my_list
         if i == search
-            i = replace
+            new_list.append(replace)
+        else
+            new_list.append(i)
     return new_list
