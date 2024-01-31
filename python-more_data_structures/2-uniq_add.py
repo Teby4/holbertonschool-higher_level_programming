f258dd2755e93113564d2d59dad25fd632e59ce1
Teby4 <felipeoliveramantero@gmail.com>
2024-01-31 06:18:40 -0800

    task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-
+    res_set = set()
+    result = 0
+    
+    for item in my_list:
+        if item not in res_set:
+            res_set.add(item)
+            result += item
+            
+    return result
