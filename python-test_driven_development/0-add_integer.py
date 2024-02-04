e5c0e25a83528dcb8310baed21cc44f80b02ccc6
root <felipeoliveramantero@gmail.com>
2024-02-04 15:29:09 -0800

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" this file contains the  add_integrer function """
-
+""" 
+this file contains the  add_integrer function
+"""
 
 def add_integer(a, b=98):
-    """ add the addition of two integrers """
-
+    """ 
+    add the addition of two integrers
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
+
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
