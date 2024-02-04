84a6e9835cb9e31ec18c3544aea57d79aa86066d
root <felipeoliveramantero@gmail.com>
2024-02-04 01:31:28 +0000

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Write a class Square that defines a square by: (based on 0-square.py)
+"""
+
+class Square:
+    """
+    square class
+    """
+    def __init__(self, size):
+        """
+        initialice square instance
+        """
+        self.__size = size 
