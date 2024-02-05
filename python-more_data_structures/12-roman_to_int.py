544d9f5f8102aa239b63a4ec266ffd0a5a43e86c
root <felipeoliveramantero@gmail.com>
2024-02-04 19:05:37 -0800

    task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    romandict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    
+    prev_value = 0
+
+    for char in roman_string:
+        current_value = romandict[char]
+
+        if current_value > prev_value:
+            result += current_value - 2 * prev_value
+        else:
+            result += current_value
+
+        prev_value = current_value
+
+    return result
