7e1e9ca09a360505c1a6eb6552457f36080fffb7
root <felipeoliveramantero@gmail.com>
2024-02-07 19:31:22 -0800

    task 4

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -69,4 +69,4 @@ class Rectangle:
         return rectangle
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.__height, self.__width)
+        return "Rectangle({}, {})".format(self.width, self.height)
