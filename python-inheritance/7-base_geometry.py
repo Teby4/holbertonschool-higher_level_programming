280c94b6abc921ca67bacb01d573e7dcc766524c
root <felipeoliveramantero@gmail.com>
2024-02-11 14:02:32 -0800

    task 7 8

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-task 6
+task 7
 """
 
 
@@ -9,4 +9,12 @@ class BaseGeometry():
     BaseGeometry Class
     """
     def area(self):
+        """raises exception"""
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates integrer"""
+        if not isinstance(value, int):
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -28,7 +28,7 @@ class Rectangle(BaseGeometry):
         """
         Init
         """
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
