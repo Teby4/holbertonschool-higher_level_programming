cd5023de33d98747ce55502c810d238316d54880
root <felipeoliveramantero@gmail.com>
2024-02-11 13:04:17 -0800

    task 2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+task 3
+"""
+
+
 def is_same_class(obj, a_class):
+    """
+    check for obj instanse of a_class
+    """
     if isinstance(obj, a_class):
         return True
     else:
