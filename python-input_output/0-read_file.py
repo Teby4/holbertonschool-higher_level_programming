caa617de169f813c06417806e446286def92e6c4
root <felipeoliveramantero@gmail.com>
2024-02-11 14:50:13 -0800

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,6 +3,9 @@
 task 0
 """
 def read_file(filename=""):
-    with open(filename) as f:
-        for line in f:
+    """
+    read and print file    
+    """
+    with open(filename, encoding="utf-8") as file:
+        for line in file:
             print(line, end="")
