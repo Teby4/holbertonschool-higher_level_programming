3180c3eb1071b6ed094ef257b38f4cb86a7cb6c5
root <felipeoliveramantero@gmail.com>
2024-02-11 12:51:07 -0800

    task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+task 1
+"""
+
+
+class MyList(list):
+    """
+    MyList class
+    """
+
+    def print_sorted(self):
+        """print the list sorted"""
+        print(sorted(self))
