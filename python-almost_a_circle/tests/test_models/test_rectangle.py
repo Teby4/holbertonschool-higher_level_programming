8ebebdfbd0e0fb5e9b1ef26daaa8e0dd74a6d646
root <felipeoliveramantero@gmail.com>
2024-02-17 05:23:11 -0800

    task 3

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -13,19 +13,42 @@ class TestRectangle(unittest.TestCase):
 
     def test_x(self):
         """test for the x variable on rectangle"""
+        obj = Base(x=36)
+        self.assertEqual(obj.x, 36)
+        with self.assertRaises(TypeError):
+            x([1, "3"])
+        with self.assertRaises(ValueError):
+            x(-9)
 
     def test_y(self):
         """test for the y variable on rectangle"""
+        obj = Base(y=36)
+        self.assertEqual(obj.y, 36)
+        with self.assertRaises(TypeError):
+            y([1, "4"])
+        with self.assertRaises(ValueError):
+            y(-8)
 
     def test_width(self):
         """test for the width variable on rectangle"""
+        obj = Base(width=36)
+        self.assertEqual(obj.width, 36)
+        with self.assertRaises(TypeError):
+            width([1, "5"])
+        with self.assertRaises(ValueError):
+            width(-6)
 
     def test_height(self):
         """test for the height variable on rectangle"""
+        obj = Base(height=35)
+        self.assertEqual(obj.height, 35)
+        with self.assertRaises(TypeError):
+            height([1, "10"])
+        with self.assertRaises(ValueError):
+            height(-4)
 
     def test_id(self):
         """ test for id variable on Rectangle"""
-
         obj = Base(id=35)
         self.assertEqual(obj.id, 35)
         obj = Base(id=0)
