806bd1456eff4319bff9f711fbd2991c86150696
root <felipeoliveramantero@gmail.com>
2024-02-17 06:08:27 -0800

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,9 +9,10 @@ class Rectangle(Base):
     """
     Rectangle class that inherits from Base.
     """
-
     def __init__(self, width, height, x=0, y=0, id=None):
-        """init of class rectangle"""
+        """
+        Constructor of the Rectangle class.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -19,68 +20,33 @@ class Rectangle(Base):
         self.y = y
 
     @property
-    def x(self):
-        """
-        x
-        """
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
-
-    @property
-    def y(self):
-        """
-        y
-        """
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
-
-    @property
     def width(self):
-        """
-        width
-        """
         return self.__width
 
     @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+    def width(self, width):
+        self.__width = width
 
     @property
     def height(self):
-        """
-        height
-        """
         return self.__height
 
     @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.__height = value
-            
-    def area(self):
-        return self.width * self.height
+    def height(self, height):
+        self.__height = height
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
