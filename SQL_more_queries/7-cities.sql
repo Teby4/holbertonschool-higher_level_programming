393e9f9d66caefc5d0a10a2172a98c838ddc834c
Teby4 <felipeoliveramantero@gmail.com>
2024-03-13 10:40:49 -0700

    task 7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
     state_id int NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY state_id REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
