42ee8c37b9992264fc59a6c61dd98a82be850747
Teby4 <felipeoliveramantero@gmail.com>
2024-03-20 08:08:24 -0700

    D

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -18,13 +18,16 @@ class Student:
     def to_json(self, attrs=None):
         """class to json"""
         my_dict = {}
-
+        isstr = 0
         if isinstance(attrs, list):
             for x in attrs:
                 if not isinstance(x, str):
-                    break
-            else:
-                for i in attrs:
+                    isstr += 1
+        else:
+            isstr += 1
+        if isstr is 0:
+            for i in attrs:
+                if hasattr(self, i)
                     my_dict[i] = getattr(self, i)
         else:
             my_dict = self.__dict__
