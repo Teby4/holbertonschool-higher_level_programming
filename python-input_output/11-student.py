9e8f8a49964a6bffe5f35dac2c3f3225ea69d252
Teby4 <felipeoliveramantero@gmail.com>
2024-03-20 08:10:50 -0700

    task

--- python-input_output/11-student.py
+++ python-input_output/11-student.py
@@ -15,7 +15,7 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-        def to_json(self, attrs=None):
+    def to_json(self, attrs=None):
         """class to json"""
         my_dict = {}
         isstr = 0
