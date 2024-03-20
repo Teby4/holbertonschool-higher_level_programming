940b119afe186bcd67fd4f540efb25adb3c2b58a
Teby4 <felipeoliveramantero@gmail.com>
2024-03-20 07:13:40 -0700

    tasks

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script that adds all arguments to a Python list.
+"""
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    mylist = load_from_json_file(filename)
+except:
+    mylist = []
+
+
+for i in sys.argv[1:]:
+    mylist.append(i)
+
+save_to_json_file(mylist, filename)
---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+task 8.
+"""
+
+import json
+
+def class_to_json(obj):
+
+    dict = json.dumps(obj)
+
+    return dict
