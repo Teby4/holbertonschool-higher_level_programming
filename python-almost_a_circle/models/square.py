6e1189f6baca77fb62d97088a6fec89c2b74a21d
root <felipeoliveramantero@gmail.com>
2024-03-21 04:00:49 -0700

    fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -28,3 +28,22 @@ class Square(Rectangle):
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+
+
+    def update(self, *args, **kwargs):
+        """
+        Assign arguments to id, width, height, x, and y attributes in order.
+        """
+        if args is not None:
+            if len(args) >= 1:
+                self.id = args[0]
+        if len(args) >= 2:
+            self.size = args[1]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
