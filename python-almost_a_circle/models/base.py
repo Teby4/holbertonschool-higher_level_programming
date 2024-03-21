a17472fcefe8d54459c566238c61707d164fb825
root <felipeoliveramantero@gmail.com>
2024-03-21 04:25:02 -0700

    tasks

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -22,6 +22,6 @@ class Base:
             
     @staticmethod
     def to_json_string(list_dictionaries):
-        if list_dictionaries is None:
-            return "[]"
+        if list_dictionaries is None or list_dictionaries == []:
+            return ("[]")
         return (json.dumps(list_dictionaries))
